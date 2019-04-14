8825b21cf0619b0761a9a4e4ed6e826bcf0067a6
Antonio Santana <antonio.santana.ir@outlook.com>
2019-04-14 14:30:03 -0700

    Update Grade.cs
    Exercise 2: Task 1b: Create the Student struct

--- Mod03/Labfiles/Starter/Exercise 2/GradesPrototype/Data/Grade.cs
+++ Mod03/Labfiles/Starter/Exercise 2/GradesPrototype/Data/Grade.cs
@@ -18,10 +18,18 @@ namespace GradesPrototype.Data
         public string Assessment { get; set; }
         public string Comments { get; set; }
 
-}
+    }
 
     // TODO: Exercise 2: Task 1b: Create the Student struct
-
+    public struct Student
+    {
+        public int StudentID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int TeacherID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
     
     // TODO: Exercise 2: Task 1c: Create the Teacher struct
 }
