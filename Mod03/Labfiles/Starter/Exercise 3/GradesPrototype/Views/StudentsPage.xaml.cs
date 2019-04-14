8661c825577c8269f089e5150ebe81362912fc9b
Antonio Santana <antonio.santana.ir@outlook.com>
2019-04-14 15:20:53 -0700

    3b user clicks on student, display student details

--- Mod03/Labfiles/Starter/Exercise 3/GradesPrototype/Views/StudentsPage.xaml.cs
+++ Mod03/Labfiles/Starter/Exercise 3/GradesPrototype/Views/StudentsPage.xaml.cs
@@ -62,6 +62,22 @@ txtClass.Text = String.Format("Class {0}", SessionContext.CurrentTeacher.Class);
         // TODO: Exercise 3: Task 3b: If the user clicks on a student, display the details for that student
         private void Student_Click(object sender, RoutedEventArgs e)
         {
+            Button itemClicked = sender as Button;
+
+            if (itemClicked != null)
+            {
+                // Find out which student was clicked
+                int studentID = (int)itemClicked.Tag;
+
+                if (StudentSelected !=null)
+                {
+                    // Find the details of the student by examining the DataContext of the Button
+                    Student student = (Student)itemClicked.DataContext;
+
+                    // Raise the StudentSelected event (handled by MainWindow to display the details for this student
+                    StudentSelected(sender, new StudentEventArgs(student));
+                }
+            }
 
         }
         #endregion
