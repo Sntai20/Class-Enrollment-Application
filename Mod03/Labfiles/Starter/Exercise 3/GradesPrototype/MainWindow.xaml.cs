f0e75294c4401ef6d991abc1353b504f49948e5a
Antonio Santana <antonio.santana.ir@outlook.com>
2019-04-14 15:14:26 -0700

    2c: Display the student name in the banner

--- Mod03/Labfiles/Starter/Exercise 3/GradesPrototype/MainWindow.xaml.cs
+++ Mod03/Labfiles/Starter/Exercise 3/GradesPrototype/MainWindow.xaml.cs
@@ -120,7 +120,8 @@ namespace GradesPrototype
             {
                 case Role.Student:
                     // TODO: Exercise 3: Task 2c: Display the student name in the banner at the top of the page
-                    
+                    // Display the student name in the banner at the top of the page
+                    txtName.Text = string.Format("Welcome {0} {1}", SessionContext.CurrentStudent.FirstName, SessionContext.CurrentStudent.LastName);
                     // Display the details for the current student
                     GotoStudentProfile();
                     break;
