0ef273d25287a3201d2165c418c2b2166abd1e71
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2019-04-21 15:24:24 -0700

    Added Unit Test
    Unit Test failed.

---
+++ Mod04/Labfiles/Starter/Exercise 2/GradeTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("GradeTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GradeTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("0647ca9a-57d8-4d64-8208-f4a2f9d99a3a")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
